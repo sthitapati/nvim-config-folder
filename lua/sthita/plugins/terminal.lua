3463a4f181039e3baddb77c8b3159882a92b62db
sthitapati <sthita.pati@gmail.com>
2025-08-26 00:01:31 +0100

    temporary fix conflict

--- lua/sthita/plugins/terminal.lua
+++ lua/sthita/plugins/terminal.lua
@@ -1,29 +1,27 @@
--- ~/.config/nvim/lua/sthita/plugins/terminal.lua
-
 return {
-  "akinsho/toggleterm.nvim",
-  event = "VeryLazy",
-  version = "*",
-  cmd = "ToggleTerm",
-  config = function()
-    require("toggleterm").setup({
-      size = 15, -- Adjust the height of the terminal pane
-      open_mapping = [[<C-j>]],
-      hide_numbers = true,
-      shade_filetypes = {},
-      shade_terminals = true,
-      shading_factor = 1,
-      start_in_insert = true,
-      insert_mappings = true,
-      persist_size = true,
-      -- Change this line to make the terminal appear at the bottom
-      direction = "horizontal",
-      close_on_exit = true,
-      shell = vim.o.shell,
-      terminals = {},
-    })
+	"akinsho/toggleterm.nvim",
+	event = "VeryLazy",
+	version = "*",
+	cmd = "ToggleTerm",
+	config = function()
+		require("toggleterm").setup({
+			size = 15, -- Adjust the height of the terminal pane
+			-- open_mapping = [[<C-j>]], -- REMOVE OR COMMENT OUT THIS LINE
+			hide_numbers = true,
+			shade_filetypes = {},
+			shade_terminals = true,
+			shading_factor = 1,
+			start_in_insert = true,
+			insert_mappings = true,
+			persist_size = true,
+			direction = "horizontal",
+			close_on_exit = true,
+			shell = vim.o.shell,
+			terminals = {},
+		})
 
-    vim.keymap.set("n", "<leader>j", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal" })
-    vim.keymap.set("t", "<leader>j", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal" })
-  end,
+		-- Your keymaps for toggling the terminal with <leader>j will now work
+		vim.keymap.set("n", "<leader>j", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal" })
+		vim.keymap.set("t", "<leader>j", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal" })
+	end,
 }
